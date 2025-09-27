77d057bae6b4a26218998369088ee0b6f7250548
bukasnatali-collab <bukas.natali96@gmail.comm>
2025-09-27 21:31:46 +0300

    Homework 4

---
+++ src/task4/Task1.java
@@ -0,0 +1,22 @@
+package task4;
+
+public class Task1 {
+    // На вход даны 3 числа. Вывести в консоль только четные числа
+
+
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 8;
+        int number3 = 10;
+
+        if (number1 % 2 == 0) {
+            System.out.println( "Четное число:" + number1);
+        }
+        if (number2 % 2 == 0) {
+            System.out.println( "Четное число:" + number2);
+        }
+        if (number3 % 2 == 0) {
+            System.out.println( "Четное число:" + number3);
+        }
+    }
+}
